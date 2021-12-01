39d600bc2985db5fc995dda9bd4f0106a1fb45ae
vedatsengul <vedat.sengul@discoverit.co.uk>
2021-12-01 15:16:47 +0000

    Folder structure update

---
+++ AdventOfCode/Day1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace AdventOfCode
+{
+    public static class Day1
+    {
+        public static void FirstQuestion()
+        {
+            var path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Input\\Day1.txt");
+            string[] lines = File.ReadAllLines(path);
+
+            var first = Convert.ToInt32(lines[0]);
+            var count = 0;
+            for (int i = 1; i < lines.Length; i++)
+            {
+                var next = Convert.ToInt32(lines[i]);
+                if (first < next)
+                {
+                    count++;
+                }
+
+                first = next;
+            }
+
+            // Keep the console window open in debug mode.
+            Console.WriteLine(count);
+            System.Console.ReadKey();
+        }
+
+        public static void SecondQuestion()
+        {
+            var path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Input\\Day1.txt");
+            string[] lines = File.ReadAllLines(path);
+
+            var firstIndex = 0;
+            var secondIndex = 1;
+            var count = 0;
+
+            while ((firstIndex + 2) < lines.Length && (secondIndex + 2 < lines.Length))
+            {
+                var first = Convert.ToInt32(lines[firstIndex]) + Convert.ToInt32(lines[firstIndex + 1]) + Convert.ToInt32(lines[firstIndex + 2]);
+                var second = Convert.ToInt32(lines[secondIndex]) + Convert.ToInt32(lines[secondIndex + 1]) + Convert.ToInt32(lines[secondIndex + 2]);
+
+                if (first < second)
+                {
+                    count++;
+                }
+
+                firstIndex++;
+                secondIndex++;
+            }
+
+            // Keep the console window open in debug mode.
+            Console.WriteLine(count);
+            System.Console.ReadKey();
+        }
+    }
+}

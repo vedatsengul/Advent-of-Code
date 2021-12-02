ae4f612451fb2e6670567156c8ca01fa4832bf90
vedatsengul <vedat.sengul@discoverit.co.uk>
2021-12-02 11:01:54 +0000

    Day 2

---
+++ AdventOfCode/Day2.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace AdventOfCode
+{
+    public static class Day2
+    {
+        public static void FirstQuestion()
+        {
+            var path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Input\\Day2.txt");
+            string[] lines = File.ReadAllLines(path);
+
+            var horizontal = 0;
+            var depth = 0;
+
+            foreach (var line in lines)
+            {
+                var splittedLine = line.Split(' ');
+                var command = splittedLine[0];
+                var distance = Convert.ToInt32(splittedLine[1]);
+
+                if (command == "forward")
+                {
+                    horizontal += distance;
+                }
+                else
+                if (command == "up")
+                {
+                    depth -= distance;
+                }
+                else
+                if (command == "down")
+                {
+                    depth += distance;
+                }
+            }
+
+            // Keep the console window open in debug mode.
+            Console.WriteLine(depth*horizontal);
+            System.Console.ReadKey();
+        }
+        public static void SecondQuestion()
+        {
+            var path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Input\\Day2.txt");
+            string[] lines = File.ReadAllLines(path);
+
+            var horizontal = 0;
+            var depth = 0;
+            var aim = 0;
+
+            foreach (var line in lines)
+            {
+                var splittedLine = line.Split(' ');
+                var command = splittedLine[0];
+                var distance = Convert.ToInt32(splittedLine[1]);
+
+                if (command == "forward")
+                {
+                    horizontal += distance;
+                    depth = depth + (aim * distance);
+                }
+                else
+                if (command == "up")
+                {
+                    aim -= distance;
+                }
+                else
+                if (command == "down")
+                {
+                    aim += distance;
+                }
+            }
+
+            // Keep the console window open in debug mode.
+            Console.WriteLine(depth * horizontal);
+            System.Console.ReadKey();
+        }
+    }
+}
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -6,7 +6,7 @@ namespace AdventOfCode
     {
         static void Main(string[] args)
         {
-            Day1.SecondQuestion();
+            Day2.SecondQuestion();
         }
     }
 
